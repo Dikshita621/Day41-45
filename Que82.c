b8987fb870b837aeae80372b76747b1202ed0b2f
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-30 20:25:45 +0530

    Create Que82.c

---
+++ Que82.c
@@ -0,0 +1,28 @@
+//Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i = 0;
+    
+    // Read the input string including spaces until newline
+    fgets(str, sizeof(str), stdin);
+    
+    // Loop through each character until newline or null terminator
+    while (str[i] != '\0' && str[i] != '\n') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+    
+    return 0;
+}
